1b010f42f9d74a9b2d7d112cb4a6d70e61c8a6b4
Chenchal Subraveti <chenchals@gmail.com>
2017-09-21 10:35:32 -0500

    Pruned useless files / functions for release joSpatial

---
+++ config/processJoule.m
@@ -0,0 +1,13 @@
+function [ nhpSessions ] = processJoule()
+%PROCESSJOULE Summary of this function goes here
+%   Detailed explanation goes here
+
+    nhpConfig.nhp = 'joule';
+    nhpConfig.srcNhpDataFolder = '/Volumes/schalllab/data/Joule';
+    nhpConfig.excelFile = '/Users/subravcr/Projects/lab-schall/schalllab-clustering/matlab/SFN_NHP_Coordinates_All.xlsx';
+    nhpConfig.nhpSheetName = 'Jo';
+    nhpConfig.outputFolder = '/Users/subravcr/Projects/lab-schall/schalllab-clustering/clustering/schalllab-spatial/testData';
+    nhpSession = processS
+    
+end
+
--- config/processSessions.m
+++ config/processSessions.m
@@ -2,12 +2,6 @@ function [ nhpSessions ] = processSessions(nhpConfig)
 %PROCESSSESSIONS Summary of this function goes here
 %   Detailed explanation goes here
 
-    nhpConfig.nhp = 'joule';
-    nhpConfig.srcNhpDataFolder = '/Volumes/schalllab/data/Joule';
-    nhpConfig.excelFile = '/Users/subravcr/Projects/lab-schall/schalllab-clustering/matlab/SFN_NHP_Coordinates_All.xlsx';
-    nhpConfig.nhpSheetName = 'Jo';
-    nhpConfig.outputFolder = '/Users/subravcr/Projects/lab-schall/schalllab-clustering/clustering/schalllab-spatial/testData';
-    
     nhp = nhpConfig.nhp;
     srcNhpDataFolder = nhpConfig.srcNhpDataFolder;
     excelFile = nhpConfig.excelFile;
