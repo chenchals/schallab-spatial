669a0d3c2c3546b9af03d44462081abff34f6dd4
Chenchal Subraveti <chenchals@gmail.com>
2017-09-21 11:15:46 -0500

    checking Id on file

--- config/processJoule.m
+++ config/processJoule.m
@@ -2,12 +2,14 @@ function [ nhpSessions ] = processJoule()
 %PROCESSJOULE Summary of this function goes here
 %   Detailed explanation goes here
 
+% $Id$
+
     nhpConfig.nhp = 'joule';
     nhpConfig.srcNhpDataFolder = '/Volumes/schalllab/data/Joule';
-    nhpConfig.excelFile = '/Users/subravcr/Projects/lab-schall/schalllab-clustering/matlab/SFN_NHP_Coordinates_All.xlsx';
+    nhpConfig.excelFile = '/Users/subravcr/Projects/lab-schall/schalllab-spatial/config/SFN_NHP_Coordinates_All.xlsx';
     nhpConfig.nhpSheetName = 'Jo';
-    nhpConfig.outputFolder = '/Users/subravcr/Projects/lab-schall/schalllab-clustering/clustering/schalllab-spatial/testData';
-    nhpSession = processS
+    nhpConfig.outputFolder = '/Users/subravcr/Projects/lab-schall/schalllab-spatial/processed';
+    nhpSessions = processSessions(nhpConfig);
     
 end
 
