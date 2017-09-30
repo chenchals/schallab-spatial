7dd806688a2b88657bf113bdd6ae3c971137ed57
Chenchal Subraveti <chenchals@gmail.com>
2017-09-29 21:27:24 -0700

    updated with xlx and darwin explore

---
+++ config/processDarwin.m
@@ -0,0 +1,27 @@
+function [ nhpSessions ] = processDarwin()
+%PROCESSJOULE Configure Joule sessions here
+%     nhpConfig is a structured variable with fields that define how to
+%     process matalb datafile for this NHP.
+% see also PROCESSSESSIONS for how to define nhpConfig 
+
+    nhpConfig.nhp = 'darwin';
+    nhpConfig.nhpSourceDir = '/Users/chenchals/Projects/lab-schall/schalllab-clustering/data/darwin';
+    nhpConfig.excelFile = '/Users/chenchals/Projects/lab-schall/schalllab-spatial/config/SFN_NHP_Coordinates_All.xlsx';
+    nhpConfig.sheetName = 'Da';
+    nhpConfig.nhpOutputDir = '/Users/chenchals/Projects/lab-schall/schalllab-spatial/processed/Darwin';
+    % a function handle for getting sessions
+    nhpConfig.getSessions = @getSessions;  
+    
+    nhpSessions = processSessions(nhpConfig);
+    
+end
+
+function [ sessions ] = getSessions(srcFolder, nhpTable)
+% Function to output the location of darwin source data files as cell array of cellstr
+%  Uses column name 'rawFilename' from the excel file used for configuration
+  rawFiles = nhpTable.rawFilename;
+  toks = regexp(rawFiles,'.*-(\d*[ab]).*','tokens');
+  sessionNames=cellfun(@(x) datestr(datenum(x{1},'mmddyya'),'yyyy-mm-dda') ,toks,'UniformOutput',false);  
+  allSessions=cellfun(@(x) dir(fullfile(srcFolder, char(x),'DSP/DSP*/*_MG*.mat')),sessionNames,'UniformOutput',false);
+  sessions = cellfun(@(x) strcat({x.folder}',filesep,{x.name}'),allSessions,'UniformOutput',false);
+end
---
+++ exploreDarwin.m
@@ -0,0 +1,53 @@
+load('da-files.mat')
+da1=daFiles{1};
+load(char(da1{25}))
+
+
+trialList = ismember(Task.TaskType,'MG')' ... % get MG trials vector
+                          & ...
+             Task.TargetLoc == targRF(im);
+
+%line 83:
+
+if (...
+        (iu > 1)...
+        && ... % sub-cond evaluated only if iu>1 is true
+        (...
+        size(...
+            spiketimes(... % Rows of spiketimes = trialList
+                      ismember(Task.TaskType,'MG')' ... % get MG trials vector
+                      & ...
+                      Task.TargetLoc == targRF(im)... % target location vector
+            ,:)...
+        ,1) ... %size(spiketimes(trialList,:),1)
+        == size(chanSpks,1) ...
+        )... %close for &&
+    )...
+    || ...
+    isempty(chanSpks) % No chanSpks
+    % end of condition
+
+
+
+end
+
+
+% line 84
+% Create Multi Unit spiketimes by concatenating matrices for:
+%   Single units 
+%   For a given channel
+%     cat Concatenate arrays
+%     cat(DIM,A,B) concatenates the arrays A and B along
+%     the dimension DIM.  
+%     cat(2,A,B) is the same as [A,B].
+% Create multi unit spikes by trial for
+%   each singleUnit 
+%   for a channel
+chanSpks = ...
+    cat(2,...
+        chanSpks,...% existing single unit spiketime matrix for curr channel
+        spiketimes(...% *next* single unit spiketime matrix for current channel
+        ismember(Task.TaskType,'MG')'...
+        & Task.TargetLoc == targRF(im),...
+        :)...
+    ); % end cat
