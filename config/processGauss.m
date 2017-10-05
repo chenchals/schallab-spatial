8aa28a20971895cf36190e5768545d52dc28b25e
Chenchal Subraveti <chenchals@gmail.com>
2017-10-05 07:14:40 -0700

    add processGauss to repo

---
+++ config/processGauss.m
@@ -0,0 +1,28 @@
+function [ nhpSessions, nhpConfig ] = processGauss()
+%PROCESSJOULE Configure Joule sessions here
+%     nhpConfig is a structured variable with fields that define how to
+%     process matalb datafile for this NHP.
+% see also PROCESSSESSIONS for how to define nhpConfig 
+
+    nhpConfig.nhp = 'gauss';
+    nhpConfig.nhpSourceDir = '/Volumes/schalllab';
+    nhpConfig.excelFile = 'SFN_NHP_Coordinates_All.xlsx';
+    nhpConfig.sheetName = 'Ga';
+    nhpConfig.nhpOutputDir = '../processed/Gauss';
+    % a function handle for getting sessions
+    nhpConfig.getSessions = @getSessions;  
+    % DataModel to use
+    nhpConfig.dataModelName = DataModel.WOLF_DATA_MODEL;
+    nhpConfig.outcome = 'Correct';
+    
+    nhpSessions = processSessions(nhpConfig);
+    
+end
+
+function [ sessions ] = getSessions(srcFolder, nhpTable)
+% Function to output the location of darwin source data files as cell array of cellstr
+%  Uses column name 'matPath' from the excel file used for configuration
+  allSessions=cellfun(@(x) dir(fullfile(srcFolder,x)),nhpTable.matPath,'UniformOutput',false);
+  sessions = cellfun(@(x) strcat({x.folder}',filesep,{x.name}'),allSessions,'UniformOutput',false);
+  sessions = sessions(~cellfun(@isempty,sessions));   
+end
