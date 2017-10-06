526b1d07f1b23aa815fa6efd4b45095cd21dcaf3
Chenchal Subraveti <chenchals@gmail.com>
2017-10-06 12:10:34 -0500

    no message

--- config/processJoule.m
+++ config/processJoule.m
@@ -10,7 +10,7 @@ function [ nhpSessions ] = processJoule()
     nhpConfig.sheetName = 'Jo';
     % Write to one dir above the config dir
     [thisDir,~,~] = fileparts(mfilename('fullpath'));    
-    nhpConfig.nhpOutputDir = fullfile(thisDir, '../processed', nhp);  
+    nhpConfig.nhpOutputDir = fullfile(thisDir, '../processed', config.nhp);  
     % a function handle for getting sessions
     nhpConfig.getSessions = @getSessions;  
     % DataModel to use
