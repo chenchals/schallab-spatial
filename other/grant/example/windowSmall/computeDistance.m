af6f9c83235470da9ffe79220d98e881fa0a83d6
Chenchal Subraveti <chenchals@gmail.com>
2017-10-30 08:07:15 -0500

    updated for movie

--- other/grant/example/windowSmall/computeDistance.m
+++ other/grant/example/windowSmall/computeDistance.m
@@ -5,6 +5,7 @@ function [ distIms, mvFrames ] = computeDistance( sdfMat, distWindow )
  nChannels = size(sdfMat,1);
  % start index of sliding window
  startInd = 1:nTimes-distWindow;
+ %startInd = (0:distWindow:nTimes-distWindow)+1;
  
  distIms = arrayfun(@(x) ...
      pdist2(sdfMat(:,x:x+distWindow-1),sdfMat(:,x:x+distWindow-1),'correlation'),...
@@ -13,19 +14,27 @@ function [ distIms, mvFrames ] = computeDistance( sdfMat, distWindow )
  
  maxVal = max(distIms(:)');
  minVal = min(distIms(:)');
+ distIms = (distIms-minVal)./(maxVal-minVal); 
+ threshold = 0.5;
  
- colorMapName = 'jet';
+ figure
+ ax = gca;
+ imagesc(distIms(:,:,1));
+ ax.NextPlot = 'replaceChildren';
+ axis tight manual
+ zlim([0 1])
  
- colorData = colormap(colorMapName);
- % Int scale
- distIms = (distIms-minVal).* ((size(colorData,1)-1)/(maxVal-minVal));
- distIms = round(distIms)+1;
- distIms(isnan(distIms))=1;
- 
- for ii = 1:size(distIms,3)
-     mvFrames(ii) = im2frame(distIms(:,:,ii),colorData);
+ colormap('cool')
+ nFrames = size(distIms,3);
+ mvFrames(nFrames) = struct('cdata',[],'colormap',[]);
+ for ii=1:nFrames
+     im = tril(distIms(:,:,ii),-1);
+     im(im<threshold) = 0;
+     %imagesc(im);
+     %mesh(im)
+     surf(im)
+     titleTxt = sprintf('Time window : %d to %d',ii, ii+distWindow);
+     title(titleTxt)
+     drawnow
+     mvFrames(ii) = getframe;
  end
-  
-  
-end
-
