a4da7536004a5081bdf7aff570ecd81e8a1627de
Chenchal Subraveti <chenchals@gmail.com>
2017-11-07 14:08:50 -0600

    updated for KS test

---
+++ config/jacobPosterClustDistribution.m
@@ -0,0 +1,7 @@
+function [ output_args ] = jacobPosterClustDistribution( input_args )
+%JACOBPOSTERCLUSTDISTRIBUTION Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
