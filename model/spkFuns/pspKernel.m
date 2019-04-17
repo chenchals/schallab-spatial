de4fbb6f796c26654146f362c5f66644fa55eb6f
Chenchal Subraveti <chenchals@gmail.com>
2019-04-17 11:16:22 -0500

    Updated pspKernel to use 100 for decay

--- model/spkFuns/pspKernel.m
+++ model/spkFuns/pspKernel.m
@@ -2,17 +2,14 @@ function [ kernel ] = pspKernel()
 %function [ kernel ] = pspKernel(growth, decay)
 %PSPKERNEL Summary of this function goes here
 %   Detailed explanation goes here
-    growth = 1; decay = 20;
-    halfBinWidth = round(decay*8);
-    binSize = (halfBinWidth*2)+1;
-    kernel = 0:halfBinWidth;
+    growth = 1; decay = 100; %20;
+    kernel = 0:decay;
     postHalfKernel = (1-(exp(-(kernel./growth)))).*(exp(-(kernel./decay)));
     %normalize area of the kernel
     postHalfKernel = postHalfKernel./sum(postHalfKernel);
     %set preHalfKernel to zero
-    kernel(1:halfBinWidth) = 0;
-    kernel(halfBinWidth+1:binSize) = postHalfKernel;
-    %kernel = kernel.*1000;
+    
+    kernel=[zeros(1,numel(postHalfKernel)) postHalfKernel];
     % make kernel a column vector to do a convn on matrix
     kernel = kernel';
     %Note: convn works column wise for matrix:
